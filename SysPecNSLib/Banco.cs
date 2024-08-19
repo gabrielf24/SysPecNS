d91f287001dd1d3e6440f789a9cb4da4e6dd99da
GABRIEL FERREIRA DE SOUZA <gabrielsouza191427@gmail.com>
2024-08-19 11:55:05 -0300

    Adição de codigos no banco de dados

--- SysPecNSLib/Banco.cs
+++ SysPecNSLib/Banco.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SysPecNSLib
 {
-    public class Banco
+    public static class Banco // classe de conexão de banco de dados 
     {
+     /// <summary>
+     /// Método abrir da classe Banco é utilizado para representar comandos SQL
+     /// no sevidor, cujos os dados foram informados na string de conexão(strconn)
+     /// </summary>
+     /// <returns>Entrega um objeto de comandos SQL, que serão executados na conexão informada </returns>
+        public static MySqlCommand Abrir() //método de conexão
+        {
+            string strconn = @"server=127.0.0.1;database=syspecdb;user=root;password=";
+            MySqlConnection cn = new  MySqlConnection(strconn);
+            MySqlCommand cmd = new ();
+            try // 
+            {
+                cn.Open();   
+                cmd.Connection = cn;
+            }
+            catch(Exception) 
+            {
+                throw;
+            }
+            return cmd;
+        }
     }
 }
