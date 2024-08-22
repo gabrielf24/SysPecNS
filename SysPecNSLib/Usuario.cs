21190f4361583551ed7b37eaebbeb199e4e393dc
GABRIEL FERREIRA DE SOUZA <gabrielsouza191427@gmail.com>
2024-08-22 11:52:35 -0300

    Classe usuario quase pronta

--- SysPecNSLib/Usuario.cs
+++ SysPecNSLib/Usuario.cs
@@ -3,10 +3,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace SysPecNSLib
 {
     public class Usuario
     {
+
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public Nivel Nivel { get; set; }
+        public bool Ativo { get; set; }
+
+        public Usuario()
+        {
+            Nivel = new();
+        }
+
+        public Usuario(string? nome, string? email, string? senha, Nivel nivel)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+        }
+
+        public Usuario(string? nome, string? email, string? senha, Nivel nivel, bool ativo)
+        {
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+
+        public Usuario(int id, string? nome, string? email, string? senha, Nivel nivel, bool ativo)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+            Nivel = nivel;
+            Ativo = ativo;
+        }
+
+        public void Inserir()
+        {
+           var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_usuario_insert";
+            cmd.Parameters.AddWithValue("spnome", Nome);
+            cmd.Parameters.AddWithValue("spemail", Email);
+            cmd.Parameters.AddWithValue("spsenha", Senha);
+            cmd.Parameters.AddWithValue("spnivel", Nivel.Id);
+            var dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                Id = dr.GetInt32(0);
+            }
+
+        }
+        public static Usuario ObterPorId(int Id)
+        {
+
+            Usuario usuario = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from usuarios where id = {id}";
+            var dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                usuario = new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2),
+                    dr.GetString(3),
+                    Nivel.ObterPorId(dr.GetInt32(4)),
+                    dr.GetBoolean(5)
+                    );
+            }
+            return usuario;
+        }
+
+        public static List<Usuario> ObterLista()
+        {
+            List<Usuario> lista = new();
+            var comandosSQL = Banco.Abrir();
+            comandosSQL.CommandType = CommandType.Text;
+            comandosSQL.CommandText = "select * from usuarios order by nome limit 10";
+            var dr = comandosSQL.ExecuteReader();
+            while(dr.Read())
+            {
+                lista.Add(
+                    new(
+                        dr.GetInt32(0),
+                        dr.GetString(1),
+                        dr.GetString(2),
+                        dr.GetString(3),
+                        Nivel.ObterPorId(dr.GetInt32(4)),
+                        dr.GetBoolean(5)
+                        )
+                    );
+            }
+            return lista;
+        }
+
+        public static Usuario EfetuarLogin(string email, string senha)
+        {
+            Usuario usuario = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from usuarios where email = {email}, and senha = md5('{senha}')";
+            var dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                usuario = new(
+                    dr.GetInt32(0),
+                    dr.GetString(1),
+                    dr.GetString(2),
+                    dr.GetString(3),
+                    Nivel.ObterPorId(dr.GetInt32(4)),
+                    dr.GetBoolean(5)
+                );
+            }
+
+            return usuario;
+        }
+        public void Atualizar()
+        {
+            // usuario: nome, senha, nível... 
+        }
+        public void Arquivar() 
+        { 
+               
+        }
+        public void Restaurar()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
