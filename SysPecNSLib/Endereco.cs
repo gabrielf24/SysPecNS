41b3ca57199e6c8ece2c07990923b1f07c0b5c00
GABRIEL FERREIRA DE SOUZA <gabrielsouza191427@gmail.com>
2024-08-30 11:54:45 -0300

    Finalização da classe clientes e criação da tabala enderecos

--- SysPecNSLib/Endereco.cs
+++ SysPecNSLib/Endereco.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +27,41 @@ namespace SysPecNSLib
             Cliente = new();
         }
 
-        public Endereco(Cliente cliente,  )
+        public Endereco(Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipo_endereco)
+        {
+            Cliente = cliente;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = Cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+        }
+
+        public Endereco(int id,Cliente cliente, string? cep, string? logradouro, string? numero, string? complemento, string? bairro, string? cidade, string? uf, string? tipo_endereco)
+        {
+            Id = id;
+            Cliente = cliente;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cidade = Cidade;
+            Uf = uf;
+            Tipo_endereco = tipo_endereco;
+        }
+
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_endereco_insert";
+            cmd.Parameters.AddWithValue("spcliente", Cliente.Id);
+        }
+
+
     }
 }
